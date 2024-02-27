cc7b29a287213b8ec4bf50db355322d10ae88f6b
Speeedman <157069619+Speeedman@users.noreply.github.com>
2024-02-27 14:06:30 -0800

    Created EnemyPatrol Script

---
+++ Tech 1 Game 1/Assets/Scripts/Enemies/EnemyPatrol.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform[] Waypoints;
+    int target;
+    public float speed;
+    //public Hp health;
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = 0;
+        transform.position = Waypoints[0].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != Waypoints[target].position)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Waypoints[target].position, speed * Time.deltaTime);
+        }
+        else
+        {
+            target = (target + 1) % Waypoints.Length;
+        }
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D other)
+     {
+    if(other.gameObject.CompareTag("Player"))
+      {
+         Health.TakeDamage(damage);
+      }
+    }*/
+}
