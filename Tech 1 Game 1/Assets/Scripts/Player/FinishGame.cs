e1364ec52430f315b9221927b505e1510095d881
Speeedman <157069619+Speeedman@users.noreply.github.com>
2024-03-26 13:29:06 -0700

    Created FinishGame Script

---
+++ Tech 1 Game 1/Assets/Scripts/Player/FinishGame.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishGame : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Movement play = other.gameObject.GetComponent<Movement>();
+            SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
